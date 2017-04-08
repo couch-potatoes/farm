50bdb7b7a8658c5c0db0d7ed66d079ef989fce17
JMH <jheld135@gmail.com>
2017-04-08 18:02:43 -0500

    Add automigrate/autoupdate boot script

---
+++ server/boot/generate-tables.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function(app, cb) {
+  /*
+  * The `app` object provides access to a variety of LoopBack resources such as
+  * models (e.g. `app.models.YourModelName`) or data sources (e.g.
+  * `app.datasources.YourDataSource`). See
+  * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
+  * for more info.
+  */
+
+  var dataSource = app.dataSources['mysql-db'];
+  /*
+  * We can pass an array of strings of models to automigrate.
+  * NOTE: automigrating will delete the model's table if it exists, so this
+  * should be done only for models that don't have a table.
+  */
+  // var tablesToMigrate = [];
+  // dataSource.automigrate(tablesToMigrate, function(err) {
+  //   if (err) return cb(err);
+  //   return cb();
+  // });
+
+  // Update all existing tables
+  dataSource.autoupdate(null, function(err) {
+    if (err) return cb(err);
+    return cb();
+  });
+};
